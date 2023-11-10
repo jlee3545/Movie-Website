641c6bf2dc70888e8ddca3db89b60c993773ddbd
AnitaSally22 <anitasallysugumar@gmail.com>
2023-11-10 12:21:56 -0500

    changes

--- java/src/main/java/com/techelevator/dao/UserWishListDao.java
+++ java/src/main/java/com/techelevator/dao/UserWishListDao.java
@@ -5,7 +5,7 @@ import com.techelevator.model.Movie;
 import java.util.List;
 
 public interface UserWishListDao {
-    List<Movie> getUserWishList();
+    List<Movie> getUserWishList(principal);
     void addToWishList(int userId, int movieId);
     int deleteFromWishList(int movieId , int userId);
 }
