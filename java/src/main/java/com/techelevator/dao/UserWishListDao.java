3bd751202ccc5ca1540114986072f3fa79864b4c
jlee3545 <123332807+jlee3545@users.noreply.github.com>
2023-11-14 14:10:24 -0500

    code

--- java/src/main/java/com/techelevator/dao/UserWishListDao.java
+++ java/src/main/java/com/techelevator/dao/UserWishListDao.java
@@ -4,11 +4,8 @@ import com.techelevator.model.Movie;
 
 import java.util.List;
 
-public interface UserMovieDao {
-    List<Movie> getUserWishList(String username);
-=======
 public interface UserWishListDao {
-    List<Movie> getUserWishList();
+    List<Movie> getUserWishList(String username);
     void addToWishList(int userId, int movieId);
     int deleteFromWishList(int movieId , int userId);
 }
--- vue/src/components/AddAndDeleteButton.vue
+++ vue/src/components/AddAndDeleteButton.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <button    
-    v-on:click="isOnWatchList?deleteMovieFromWatchList():addMovieToWatchlist()">{{isOnWatchList?"Delete From WatchList":"Add to Watchlist"}}</button>
-       {{isOnWatchList}}
+    <button v-on:click="isOnWatchList ? deleteMovieFromWatchList() : addMovieToWatchlist()">
+    {{ isOnWatchList ? "Delete From WatchList":"Add to Watchlist"}}</button>
   </div>
 </template>
 
@@ -10,49 +9,39 @@
 
 import APIService from '../services/APIService';
 export default {
-    props: { movie: Object },
-    methods:{
-      addMovieToWatchlist(){
-        APIService.addToWishlist(this.$route.params.id).then(
-          window.alert("Movie added to watchlist")
-        )
-      },
+  props: { currentMovie: Object },
+  methods: {
+    addMovieToWatchlist() {
+      APIService.addToWishlist(this.$route.params.id).then(
+        window.alert("Movie added to watchlist")
+      )
+    },
 
-      deleteMovieFromWatchList(){
-        APIService.deleteFromWishlist(this.$route.params.id).then(
-          window.alert("Movie deleted from list")
-        )
-      }
-      },
-      computed:{
-    //    isOnWatchList(){
-    //     for(let i = 0; i < this.$store.state.watchList.length; i++){
-    //       if (this.$store.state.movie.movieId == this.$store.state.watchList[i].movieId){
-    //         return true;
-    //       }
-         
-    //     }
-    //      return false;
-    //   }
-
-    isOnWatchList(){
-        let movieIndex = 0;
-        for(let i=0; i<this.$store.state.movies.length; i++){
-           if(this.$store.state.movies[i].movieId == this.movie.movieId){
-                movieIndex = i;
-           }
-        }
-        for(let i = 0; i < this.$store.state.watchList.length; i++){
-          if (this.$store.state.movies[movieIndex].movieId == this.$store.state.watchList[i].movieId){
-            return true;
-          }
-        }
-         return false;
+    deleteMovieFromWatchList() {
+      APIService.deleteFromWishlist(this.$route.params.id).then(
+        window.alert("Movie deleted from list")
+      )
     }
-}
+  },
+  computed: {
+
+    isOnWatchList() {
+      let movieIndex = 0;
+      for (let i = 0; i < this.$store.state.movies.length; i++) {
+        if (this.$store.state.movies[i].movieId == this.currentMovie.movieId) {
+          movieIndex = i;
+        }
+      }
+      for (let i = 0; i < this.$store.state.watchList.length; i++) {
+        if (this.$store.state.movies[movieIndex].movieId == this.$store.state.watchList[i].movieId) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
